9d6266994af2e6dcd02ee561895f12a63450cf72
Jevoughn Wisdom <jevoughnwisdom@gmail.com>
2020-07-15 02:24:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,6 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase()
     return "NO, NOT SINCE 1938!"
   end
-end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
